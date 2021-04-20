8b19e047bbfa34f4a4e137819d8f0d5b96e46ce6
Alex White <alex_p_white@yahoo.co.uk>
2021-04-20 12:38:57 +0100

    added lambda functions for monitoring tide information

---
+++ fetch/src/main.rs
@@ -0,0 +1,34 @@
+// https://adevait.com/rust/deploying-rust-functions-on-aws-lambda
+
+use rusoto_core::Region;
+use rusoto_s3::{S3, S3Client};
+use lambda_runtime::{error::HandlerError, lambda, Context};
+use std::error::Error;
+use futures::executor;
+use chrono::{Utc, Datelike};
+
+mod s3;
+mod tides;
+
+struct LambdaRequest;
+struct LambdaResponse;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    lambda!(lambda_handler);
+    Ok(())
+}
+
+fn lambda_handler(_e: LambdaRequest, _c: Context) -> Result<LambdaResponse, HandlerError> {
+    let client = S3Client::new(Region::EuWest1);
+    executor::block_on( process_work(client) );
+    Ok(LambdaResponse {})
+}
+
+async fn process_work(client: impl S3) {
+    // let locations = tides::admiralty_locations().await.unwrap();
+    // put_item(client, "tides/locations.json".to_owned(), locations).await;
+    let tides = tides::admiralty_tidal_events(tides::bosham_location_id(), 7).await.expect("To have had some tide data returned");
+    let now = Utc::now();
+    let key = format!("tides/{year}/{month}/{day}", year = now.year(), month = now.month0(), day = now.day0());
+    s3::put_object(client, key, tides).await;
+}
---
+++ fetch/src/tides.rs
@@ -0,0 +1,47 @@
+// https://www.admiralty.co.uk/digital-services/data-solutions/uk-tidal-api
+
+use reqwest::{Client, header};
+
+pub fn bosham_location_id() -> String {
+    std::env::var("BoshamId").expect("BoshamId should be available as an environment variable")
+}
+fn subscription_key() -> String {
+    std::env::var("SubscriptionKey").expect("Subscription key should be available as an environment variable")
+}
+
+fn admiralty_stations_url() -> String {
+    "https://admiraltyapi.azure-api.net/uktidalapi/api/V1/Stations".to_owned()
+}
+
+fn admiralty_tidal_events_url(station_id: String, duration: i32) -> String {
+    format!("https://admiraltyapi.azure-api.net/uktidalapi/api/V1/Stations/{stationId}/TidalEvents?{duration}", 
+        stationId = station_id, 
+        duration = duration)
+}
+
+fn admiralty_headers() -> header::HeaderMap {
+    let mut headers = header::HeaderMap::new();
+    let auth_value = header::HeaderValue::from_str(&subscription_key()).unwrap();
+    auth_value.set_sensitive(true);
+    headers.insert("Ocp-Apim-Subscription-Key", auth_value);
+    headers
+}
+
+pub async fn admiralty_locations() -> reqwest::Result<String> {
+    let client = Client::builder().default_headers(admiralty_headers()).build()?;
+    let response = client.get(&admiralty_stations_url()).send().await?;
+    response.text().await
+}
+
+pub async fn admiralty_location(id: String) -> reqwest::Result<String> {
+    let client = Client::builder().default_headers(admiralty_headers()).build()?;
+    let url = format!("{domain}?{stationId}", domain = &admiralty_stations_url(), stationId = id);
+    let response = client.get(url).send().await?;
+    response.text().await
+}
+
+pub async fn admiralty_tidal_events(id: String, duration: i32) -> reqwest::Result<String> {
+    let client = Client::builder().default_headers(admiralty_headers()).build()?;
+    let response = client.get(&admiralty_tidal_events_url(id, duration)).send().await?;
+    response.text().await
+}
