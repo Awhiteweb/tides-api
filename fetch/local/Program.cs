b28c90bb903996764c92e9fe721c412828e0068e
Alex White <alex_p_white@yahoo.co.uk>
2024-11-06 15:56:10 +0000

    added local console project for testing

---
+++ fetch/local/Program.cs
@@ -0,0 +1,11 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.SimpleNotificationService;
+using DailyTide;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var app = new App(new HttpClient(), new AmazonS3Client(RegionEndpoint.EUWest1), new AmazonSimpleNotificationServiceClient(RegionEndpoint.EUWest1));
+
+await app.Run("0068");
